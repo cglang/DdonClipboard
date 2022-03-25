6ddbddf2ab877f0c1a0eedfd0a838ef74093156a
cglang <adolphlanglang@163.com>
2022-03-25 18:03:17 +0800

    乱改了点东西

--- DdonTcpClient/Program.cs
+++ DdonTcpClient/Program.cs
@@ -5,7 +5,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using DdonSocket;
 using DdonSocket.Extra;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace DdonTcpClient
 {
@@ -13,24 +15,40 @@ namespace DdonTcpClient
     {
         static async Task Main(string[] args)
         {
-            //var services = new ServiceCollection();
+            // 从appsettings.json文件中读入日志的配置信息
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-            //var provider = services.BuildServiceProvider();
-
-            var ddonTcpClient = new DdonSocketClient<DdonSocketHandler>("127.0.0.1", 5003);
-
-            ddonTcpClient.StartRead();
-
-            Console.WriteLine($"被分配Id:{ddonTcpClient.ClientId}");
-
-            while (true)
+            var services = new ServiceCollection();
+            services.AddLogging(x =>
             {
-                Console.Write("输入目标客户端Id:");
-                string sendClientIdText = Console.ReadLine() ?? string.Empty;
-                var sendClientId = Guid.Parse(sendClientIdText);
-                var sendData = Console.ReadLine();
-                await ddonTcpClient.SendStringAsync($"{sendData}", sendClientId);
-            }
+                //x.SetMinimumLevel(LogLevel.Debug);
+                x.AddConfiguration(configuration);
+                x.AddConsole(); // 将日志输出到控制台
+            });
+            var provider = services.BuildServiceProvider();
+
+            var log = provider.GetRequiredService<ILogger<Program>>();
+            log.LogInformation("testi");
+            log.LogDebug("testd");
+            log.LogWarning("w");
+            log.LogError("e");
+
+            //var ddonTcpClient = new DdonSocketClient<DdonSocketHandler>("127.0.0.1", 5003);
+
+            //ddonTcpClient.StartRead();
+
+            //Console.WriteLine($"被分配Id:{ddonTcpClient.ClientId}");
+
+            //while (true)
+            //{
+            //    Console.Write("输入目标客户端Id:");
+            //    string sendClientIdText = Console.ReadLine() ?? string.Empty;
+            //    var sendClientId = Guid.Parse(sendClientIdText);
+            //    var sendData = Console.ReadLine();
+            //    await ddonTcpClient.SendStringAsync($"{sendData}", sendClientId);
+            //}
         }
     }
 
